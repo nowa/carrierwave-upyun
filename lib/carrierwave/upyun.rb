cdf1366bab59a0327f50225c1bc9f9e435026c12
Nowa <nowazhu@gmail.com>
2011-11-03 08:03:34 +0800

    -

--- lib/carrierwave/upyun.rb
+++ lib/carrierwave/upyun.rb
@@ -1,7 +1,7 @@
 require "carrierwave/storage/upyun"
 require "carrierwave/upyun/configuration"
 CarrierWave.configure do |config|
-  config.storage_engines.merge!({:tfs => "CarrierWave::Storage::UpYun"})
+  config.storage_engines.merge!({:upyun => "CarrierWave::Storage::UpYun"})
 end
 
 CarrierWave::Uploader::Base.send(:include, CarrierWave::UpYun::Configuration)
