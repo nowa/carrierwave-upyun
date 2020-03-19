473330a7490be30edbf74d0b900678855d9e127f
Lucia <luciacachn@gmail.com>
2020-03-19 11:57:30 +0800

    Optimize headers method (#38)

--- lib/carrierwave/storage/upyun/file.rb
+++ lib/carrierwave/storage/upyun/file.rb
@@ -97,7 +97,7 @@ module CarrierWave::Storage
 
       def headers
         @headers ||= begin
-          conn.get(escaped_path).headers
+          conn.head(escaped_path).headers
         rescue Faraday::ClientError
           {}
         end
